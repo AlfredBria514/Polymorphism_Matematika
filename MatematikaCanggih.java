c11f68470dfbb11f2adfd212c26b27f0b175aa87
Alfredbria21 <alfredbria21@gmail.com>
2024-12-05 17:41:48 +0700

    Add files via upload

---
+++ MatematikaCanggih.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Object;
+
+/**
+ *
+ * @author tweet
+ */
+public class MatematikaCanggih extends Matematika {
+    // Method baru untuk menerima 3 parameter bertipe double
+    public double pertambahan(double a, double b, double c) {
+        return a + b + c;
+    }
+}
